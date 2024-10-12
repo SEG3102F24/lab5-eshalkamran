a11e3145d318f1d92f7e6a81041c78496c240ba0
Eshal Kamran <ekamr048@uottawa.ca>
2024-10-12 13:16:41 -0400

    Initial commit with Firebase and Firestore setup

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    firebase: {
+      apiKey: "AIzaSyCnXpUoZzUJS1FLuNbE8ZV0bLODwhcUku4",
+      authDomain: "employeedatabase-60bb0.firebaseapp.com",
+      projectId: "employeedatabase-60bb0",
+      storageBucket: "employeedatabase-60bb0.appspot.com",
+      messagingSenderId: "50519211599",
+      appId: "1:50519211599:web:21cab851001bea0bc6b43a"
+    },
+    production: false
+  };
+  
